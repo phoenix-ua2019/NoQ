163b74658e8f89895185f20b573f3117b0f428f7
hudanov <scalesymmetry@gmail.com>
2019-04-30 21:46:07 +0300

    min sdk changed to 25 form 26

--- app/src/main/java/ua/lviv/iot/phoenix/noq/adapters/MealAdapter.java
+++ app/src/main/java/ua/lviv/iot/phoenix/noq/adapters/MealAdapter.java
@@ -1,18 +1,64 @@
 package ua.lviv.iot.phoenix.noq.adapters;
 
-import android.widget.ArrayAdapter;
-import android.app.Activity;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-import java.util.ArrayList;
+import java.util.List;
 
+import ua.lviv.iot.phoenix.noq.R;
 import ua.lviv.iot.phoenix.noq.models.Meal;
 
-public class MealAdapter extends ArrayAdapter<Meal> {
-    MealAdapter(Activity context, ArrayList<Meal> meals) {
+public class MealAdapter extends RecyclerView.Adapter<MealAdapter.MyViewHolder> {
+
+    private List<Meal> mealList;
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        public ImageView mealPicture;
+        public TextView mealName, selectedQuantity;
+
+        public MyViewHolder(View view) {
+            super(view);
+            mealName = (TextView) view.findViewById(R.id.name_of_meal);
+            selectedQuantity = (TextView) view.findViewById(R.id.selected_quantity);
+        }
+    }
+
+    public MealAdapter(List<Meal> mealList) {
+        this.mealList = mealList;
+    }
+
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.menu_list_item, parent, false);
+
+        return new MyViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, int position) {
+        Meal meal = mealList.get(position);
+        holder.mealName.setText(meal.getMealName());
+        if (meal.getSelectedQuantity() > 0) {
+            holder.selectedQuantity.setText(meal.selectedQuantityToString());
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return mealList.size();
+    }
+}
+
+    /*MealAdapter(Activity context, ArrayList<Meal> meals) {
         super(context, 0, meals);
     }
 
-    /*@Override
+    @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         View listItemView = convertView;
         if (listItemView == null) {
@@ -36,4 +82,4 @@ public class MealAdapter extends ArrayAdapter<Meal> {
         );
         return listItemView;
     }*/
-}
+
