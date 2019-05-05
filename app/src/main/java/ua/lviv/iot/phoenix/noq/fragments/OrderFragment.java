18e5833d9b659ea7f09e2c249707f6240231b0c3
Roman Spodaryk <roman.spodaryk@ukr.net>
2019-05-05 17:42:08 +0300

    took away BNV, create test button to show that we can go to next fragment

--- app/src/main/java/ua/lviv/iot/phoenix/noq/fragments/OrderFragment.java
+++ app/src/main/java/ua/lviv/iot/phoenix/noq/fragments/OrderFragment.java
@@ -15,15 +15,10 @@ import ua.lviv.iot.phoenix.noq.R;
 public class OrderFragment extends Fragment {
 
 
-    public OrderFragment() {
-        // Required empty public constructor
-    }
-
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_order, container, false);
     }
 
